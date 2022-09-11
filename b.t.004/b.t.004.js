406b51c17acb64692a9b14ad6bfbf607d78d226f
mohsen <mohsen.bahrami7690@gmail.com>
2022-09-11 17:55:14 +0430

    b.t.004-complate

--- b.t.004/b.t.004.js
+++ b.t.004/b.t.004.js
@@ -1,9 +1,19 @@
 document.querySelector('.login').addEventListener("click", checkAccess);
 function checkAccess(e) {
-    e.target.innerHTML = "wrong"
-    e.target.classList.add('wrong');
-    e.target.addEventListener('animationend', ()=>{
-        e.target.innerHTML = "login";
-        e.target.classList.remove('wrong');
-    })
+    let login = false;
+    if (login == true) {
+        e.target.innerHTML = "&#9989;"
+        e.target.classList.add('success');
+        e.target.addEventListener('animationend', () => {
+            e.target.innerHTML = "login";
+            e.target.classList.remove('success');
+        })
+    } else {
+        e.target.innerHTML = "wrong"
+        e.target.classList.add('wrong');
+        e.target.addEventListener('animationend', () => {
+            e.target.innerHTML = "login";
+            e.target.classList.remove('wrong');
+        })
+    }
 };
